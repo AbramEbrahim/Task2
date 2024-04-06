0b67b85dea035ccfcbafe1f6c573d4258c0521c8
twins <twins@TWINS>
2024-04-06 22:23:39 +0200

    API task

---
+++ task2/Controllers/API/BlogsController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using task2.Models;
+using task2.Models.data;
+
+namespace task2.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController(ApplicationDbContext _db) : ControllerBase
+    {
+
+        [HttpGet]
+        public List<Blog> getBlog()
+        {
+            var blogs = _db.blogs.Include(m => m.type).ToList();
+            return blogs;
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Blog> getBlog(int id)
+        {
+        if (id <= 0) { return BadRequest(); }
+            var blog = _db.blogs.Include(m => m.type).FirstOrDefault(m => m.Id == id);
+            if (blog == null) { return NotFound(); }
+            return Ok(blog);
+        }
+
+        [HttpPost]
+        public ActionResult<Blog> Addblog(Blog blog)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            _db.blogs.Add(blog);
+            _db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBLog(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+            var blog = _db.blogs.Include(x => x.type).FirstOrDefault(m => m.Id == id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            _db.blogs.Remove(blog);
+            _db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBlog(Blog blog)
+        {
+            if (blog.Id <= 0 || !ModelState.IsValid)
+            { return BadRequest(); }
+
+            var blog2 = _db.blogs.Include(_ => _.type).FirstOrDefault(m => m.Id == blog.Id);
+            if (blog2 == null) { return NotFound(); }
+
+            blog2.Name = blog.Name;
+            blog2.Description = blog.Description;
+            blog2.TypeId = blog.TypeId;
+
+            _db.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ task2/Controllers/API/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using task2.Models;
+using task2.Models.data;
+
+namespace task2.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(ApplicationDbContext _db) : ControllerBase
+    {
+
+        [HttpGet]
+        public List<Product> getProduct()
+        {
+            var products = _db.Products.Include(m => m.company).ToList();
+            return products;
+
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Product> getProduct(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+            var products = _db.Products.Include(m => m.company).FirstOrDefault(m => m.Id == id);
+            if (products == null)
+                return NotFound();
+            return Ok(products);
+        }
+
+        
+        [HttpPost]
+        public IActionResult AddProduct( Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            _db.Products.Add(product);
+            _db.SaveChanges();
+            return Ok();
+
+        }
+        [Route("del")]
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            if (id<= 0)
+            {
+                return BadRequest();
+            }
+            var product = _db.Products.Include(x => x.company).FirstOrDefault(m => m.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct(Product product)
+        {
+            if(product.Id<=0 || !ModelState.IsValid)
+            {  return BadRequest(); }
+
+            var product2 = _db.Products.Include(_ => _.company).FirstOrDefault(m => m.Id == product.Id);
+            if (product2 == null) {return NotFound(); }
+            product2.Name= product.Name;
+            product2.Description= product.Description;
+            product2.price = product.price;
+            product2.CompanyId = product.CompanyId;
+            product2.EnablSize= product.EnablSize;
+            _db.SaveChanges();
+            return Ok();
+        }
+    }
+}
